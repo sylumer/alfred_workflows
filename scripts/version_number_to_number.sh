f035acbdbb4d2e8cfaa893de55dfcbc2a11e2f06
sylumer <>
2025-07-19 17:16:29 +0100

    Initial version number to number script

---
+++ scripts/version_number_to_number.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+verno=1.7.0
+
+# Split the version string by periods using simple string operations
+major=$(echo $verno | cut -d'.' -f1)
+minor=$(echo $verno | cut -d'.' -f2)
+patch=$(echo $verno | cut -d'.' -f3)
+
+# Calculate the numeric representation
+# Major * 1,000,000 + Minor * 1,000 + Patch * 1
+numeric_version=$((major * 1000000 + minor * 1000 + patch * 1))
+
+# Output the result
+echo $numeric_version
